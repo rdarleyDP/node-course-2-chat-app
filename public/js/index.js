58c05c0678407633a45780e008cb5611e7cd31a8
Robin Darley <rdarley@developerprogram.com>
2017-10-02 22:38:23 -0500

    Add timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -11,19 +11,20 @@ socket.on('disconnect', function () {
 
 // Listen for newMessage
 socket.on('newMessage', function (message) {
-    console.log('New Message!', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     
     $('#messages').append(li);
 });
 
 // Listen for newLocationMessage
 socket.on('newLocationMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
     var a = $(`<a target="_blank">My Current Location</a>`);
     
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     
     li.append(a);
